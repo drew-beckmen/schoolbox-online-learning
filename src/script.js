980e0dbe752d02f01ec1e13bba6a00c84fe77673
Drew Beckmen <drew.beckmen@yale.edu>
2020-07-21 14:49:28 -0400

    sketched out basic menu and nav bar

--- src/script.js
+++ src/script.js
@@ -1,5 +1,50 @@
-const ul = document.querySelector("ul#courses")
+const main = document.getElementById("main-content")
 const baseURL = "http://localhost:3000/api/v1/"
+
+class Enrollment {
+    constructor(id, data) {
+        this.id = id 
+        this.link = data.link 
+        this.user_id = data.user_id
+        this.course_id = data.course_id
+    }
+
+    async render() {
+        let {title, description, platform_id} = await Course.fetchById(this.course_id)
+        let {name} = await Platform.fetchById(platform_id)
+        const course = document.createElement("div")
+        const h3 = document.createElement("h3")
+        h3.innerText = ` 🏫 Course: ${title}`
+        const desc = document.createElement("h5")
+        desc.innerText = `Description: ${description}`
+        const platform = document.createElement("h5")
+        platform.innerText = `Platform: ${name}`
+        const btn = document.createElement("button")
+        btn.id = this.id 
+        btn.innerText = "View Course"
+        btn.className = "btn btn-outline-info"
+        course.append(h3, desc, platform, btn)
+        main.append(course)
+    }
+}
+
+class Course {
+    static fetchById(id) {
+        return fetch(baseURL + `courses/${id}`)
+        .then(resp => resp.json())
+        .then(obj => obj.data.attributes)
+    }
+}
+
+class Platform {
+    static fetchById(id) {
+        return fetch(baseURL + `platforms/${id}`)
+        .then(resp => resp.json())
+        .then(obj => obj.data.attributes)
+    }
+}
+
+
 
 function addCourses(enrollment_ids) {
     enrollment_ids.forEach(id => addCourse(id))
@@ -8,28 +53,16 @@ function addCourses(enrollment_ids) {
 function addCourse(id) {
     fetch(baseURL + `enrollments/${id}`)
     .then(resp => resp.json())
-    .then(obj => getCourseName(obj.data.attributes.course_id, obj.data.attributes.link))
-}
-
-function getCourseName(id, link) {
-    fetch(baseURL + `courses/${id}`)
-    .then(resp => resp.json())
-    .then(obj => {
-        const li = document.createElement("li")
-        console.log(link)
-        const a = document.createElement("a")
-        a.href = link
-        console.log(a) 
-        a.innerText = "Go To Course"
-        li.innerText = obj.data.attributes.title + ": " + obj.data.attributes.description
-        ul.append(li, a)
+    .then(obj =>{
+        let e = new Enrollment(obj.data.id, obj.data.attributes)
+        e.render() //add it to the DOM
     })
 }
 
 function loadUserCourses(id) {
     fetch(baseURL + `users/${id}`)
     .then(resp => resp.json())
-    .then(obj => addCourses(obj["data"]["relationships"]["enrollments"]["data"].map(itm => itm.id)))
+    .then(obj => addCourses(obj.data.relationships.enrollments.data.map(itm => itm.id)))
 }
 
 loadUserCourses(1)
